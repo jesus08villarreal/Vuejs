18dc281d14e76995ea1ae41f01fa8c689058fc74
Jesus Villarreal <jesusvs1100@gmail.com>
2023-02-27 00:58:52 -0600

    avances nocheriegos

--- primer_vue/src/App.vue
+++ primer_vue/src/App.vue
@@ -1,33 +1,88 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, computed} from 'vue'
+
+const positivo = ref(true);
+const color = ref("color: green");
+const counter = ref(1);
+
+const incrementTW = () => {
+    
+    counter.value=counter.value+20
+
+}
 
 const increment = () => {
     
     counter.value++
 
 }
+const decrement = () => {
+    
+    counter.value--
 
-const counter = ref(0);
+}
 
+const resetNumber = () => {
+    
+    counter.value = 0;
+
+}
+const classCounter = computed(() => {
+    if(counter.value>20){
+        return 'bigger'
+    }
+
+    if(counter.value<0){
+        return 'negative'
+    }
+    if(counter.value===0){
+        return 'zero'
+    }
+    if(counter.value>0){
+        return 'positive'
+        
+    }
+})
 </script>
 
 <template>
- 
-    
-       <template type="1"  v-for="(fruta) in arrayFrutas" :key="fruta.name">
-        <li v-if="fruta.price>0">
-         -- {{ fruta.name }} -- {{ fruta.price }} -- {{ fruta.description }}
-        </li>
-           </template> 
+
+    <h2 :class="classCounter">
+        {{ counter }}
+    </h2>
             
-    <button @click="increment">Hol!</button>
-    <h2>{{ counter }}</h2>
+    <button @click="increment">Incrementar</button>
+    <button @click="incrementTW">Incrementar 20</button>
+    <button @click="decrement">Disminuir</button>
+    <button @click="resetNumber">Resetear</button>
 
 </template>
 <style>
-    h1 {
-        color: red;
-    }
+.positive { 
+    color: green;
 
+}
+.negative {
+    color: blue;
+}
+.negative:hover {
+    color: red;
+    font-size: 1.6rem;
+    transition: 3s
+}
+.zero {
+    color: white;
+    
+}
+.zero:hover {
+    color: white;
+    font-size: 3rem;
+    transition: 2s;
+}
+
+.bigger {
+    color: gold;
+    font-size: 4rem;
+}
 
 </style>
