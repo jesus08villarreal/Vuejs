3651de92ee76a3e06bc968cdfe98255cf14926e6
Jesus Villarreal <jesusvs1100@gmail.com>
2023-02-26 23:46:45 -0600

    Hola estoy acualizando desde terminal

--- primer_vue/src/App.vue
+++ primer_vue/src/App.vue
@@ -1,42 +1,6 @@
 <script setup>
 import {ref} from 'vue'
 
-
-var name = "Jesus Villarreal";
-const ColorBlue = "color: blue";
-const correcto = ["color: green", "CORRECTO", "respuesta correcta"];
-const incorrecto = ["color: red", "INCORRECTO", "RESPUESTA INCORRECTA"];
-const respuesta = true;
-
-const arrayFrutas =[
-    {
-        name: "manzana",
-        price: 20,
-        description: "una manzana"
-    },
-    {
-        name: "zanahoria",
-        price: 2,
-        description: "una manzana"
-    },
-    {
-        name: "Espiruleta",
-        price: 12,
-        description: "una manzana"
-    },
-    {
-        name: "cebolla",
-        price: 23,
-        description: "una manzana"
-    },
-    {
-        name: "carne",
-        price: 43,
-        description: "una manzana"
-    },
-  
-    
-]
 const increment = () => {
     
     counter.value++
@@ -49,18 +13,12 @@ const counter = ref(0);
 
 <template>
  
-    <h3 :style="`${correcto[0]}`">{{name}} </h3>
-    <h2 v-if="respuesta" :style="correcto[0]"> {{ correcto[1] }}</h2>
-    <h2 v-if="!respuesta" :style="incorrecto[0]"> {{ incorrecto[1] }}</h2>
-    <p v-if="!respuesta" :style="incorrecto[0]"> {{ incorrecto[2] }}</p>
-    <p v-if="respuesta" :style="correcto[0]"> {{ correcto[2] }}</p>
-
     
        <template type="1"  v-for="(fruta) in arrayFrutas" :key="fruta.name">
         <li v-if="fruta.price>0">
          -- {{ fruta.name }} -- {{ fruta.price }} -- {{ fruta.description }}
         </li>
-            </template> 
+           </template> 
             
     <button @click="increment">Hol!</button>
     <h2>{{ counter }}</h2>
