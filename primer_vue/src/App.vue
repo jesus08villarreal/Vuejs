71526e10d35c6a4af8ef2b0cecd230494d517a30
Jesus Villarreal <jesusvs1100@gmail.com>
2023-02-27 15:46:56 -0600

    Aqui ya hice la practica completa pero esta pendiente hacer el generador de lista reutilizable, lanza problema al agregar mas al array.

--- primer_vue/src/App.vue
+++ primer_vue/src/App.vue
@@ -5,19 +5,19 @@ const positivo = ref(true);
 const color = ref("color: green");
 const counter = ref(0);
 const ArrayFavoritos = []
-const buleano = ref(false)
+let checker1 = ref(false)
 let indice
+const checker2 = ref(false);
 
 
 const showList = () => {
-    for(indice in ArrayFavoritos) {
-        console.log(ArrayFavoritos[indice])
-     }
+    checker2.value = true
 }
 
 const favNumber = () => { //array que guarda los numeros agregados a favorios y no deja agregar el mismo numero si ya se agrego una vez.
-    let checker =ArrayFavoritos.indexOf(counter.value) //Solucionado con indexOf podemos saber si existe un valor dentro de una arreglo, si es que no existe entonces nos regresa -1
-    if (checker!=-1){                                   //Si si existe nos regresa como valor el indice o posicion de donde se encuentra este valor.
+    const checker =ArrayFavoritos.indexOf(counter.value) //Solucionado con indexOf podemos saber si existe un valor dentro de una arreglo, si es que no existe entonces nos regresa -1
+    if (checker!=-1){ 
+        checker1.value=true                                  //Si si existe nos regresa como valor el indice o posicion de donde se encuentra este valor.
         console.log("Ese numero ya esta agregado")
     }else{
         ArrayFavoritos.push(counter.value)
@@ -25,18 +25,18 @@ const favNumber = () => { //array que guarda los numeros agregados a favorios y 
 }
 
 const increment = () => {
-    
+    checker1.value = false
     counter.value++
 
 }
 const decrement = () => {
-    
+    checker1.value = false
     counter.value--
 
 }
 
 const resetNumber = () => {
-    
+    checker1.value = false
     counter.value = 0;
 
 }
@@ -65,10 +65,15 @@ const classCounter = computed(() => {
     </h2>
             
     <button @click="increment">Incrementar</button>
-    <button @click="favNumber">Agregar a favoritos</button>
+    <button @click="favNumber" :disabled="checker1">Agregar a favoritos</button>
     <button @click="decrement">Disminuir</button>
     <button @click="resetNumber">Resetear</button>
     <button @click="showList">Mostrar Lista</button>
+    <ul v-if="checker2===true">
+        <li v-for="(numero,indice) in ArrayFavoritos" :key="numero">
+        {{ ArrayFavoritos[indice] }}
+        </li>
+            </ul>
     
 </template>
 <style>
